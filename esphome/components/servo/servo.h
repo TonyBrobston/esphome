e0d4e65f97cd195bcf77de790a4af15c8d7d9fe5
Tony Brobston <anthonybrobston@live.com>
2024-02-18 20:39:55 -0600

    Revert "Remove set_level(0.0f) from detach"
    This reverts commit 5ef6823de92479ecb89b27cfad2fc8b8e3b6f11a.

--- esphome/components/servo/servo.h
+++ esphome/components/servo/servo.h
@@ -20,6 +20,7 @@ class Servo : public Component {
   void internal_write(float value);
   void detach() {
     this->state_ = STATE_DETACHED;
+    this->output_->set_level(0.0f);
   }
   void setup() override {
     float v;
